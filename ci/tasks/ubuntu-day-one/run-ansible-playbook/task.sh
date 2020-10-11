613d4fb5f2a3c9a1716f70847a2e6ce98295705b
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2020-10-11 08:28:07 +0000

    Add dotfiles and some other packages

--- ci/tasks/ubuntu-day-one/run-ansible-playbook/task.sh
+++ ci/tasks/ubuntu-day-one/run-ansible-playbook/task.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 
 export VM_IP_ADDRESS=$(cat vm-ip-address/ip.txt)
+export INPUT_PLAYBOOK_DIR="ubuntu-ansible-playbook/ansible/ubuntu/"
+export REQUIREMENTS_YAML="${INPUT_PLAYBOOK_DIR}/requirements.yml"
+export PLAYBOOK_YAML="${INPUT_PLAYBOOK_DIR}/playbook.yml"
 
 TMPDIR=""¬
 TMPDIR=$(mktemp -d -t dynamic_inventory.XXXXXX)
@@ -26,4 +29,5 @@ ansible-inventory -i "${INVENTORY_FILE}" --list
 ansible -i "${INVENTORY_FILE}" -m ping tools
 
 ansible-galaxy collection install matthewcosgrove.tools_vm
-ansible-playbook -i "${INVENTORY_FILE}" ubuntu-ansible-playbook/ansible/ubuntu/playbook.yml
+ansible-galaxy install -r "${REQUIREMENTS_YAML}"
+ansible-playbook -i "${INVENTORY_FILE}" "${PLAYBOOK_YAML}"
